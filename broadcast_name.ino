b54f2bb2175d1d6e6490b9ecadfec2ff43276553
Daniel McCool <mccool@opower.com>
2013-08-07 16:56:17 -0700

    first commit
    files for running the d-light-staff and a mock loop for sending data as though 10 staffs were broadcasting.

---
+++ broadcast_name.ino
@@ -0,0 +1,27 @@
+void setup()
+{
+  Serial1.begin(9600);
+}
+void loop()
+{
+  Serial1.println("<01>");
+  delay(1000);
+  Serial1.println("<02>");
+  delay(1000);
+  Serial1.println("<03>");
+  delay(1000);
+  Serial1.println("<04>");
+  delay(1000);
+  Serial1.println("<05>");
+  delay(10000);
+  Serial1.println("<06>");
+  delay(5000);
+  Serial1.println("<07>");
+  delay(8000);
+  Serial1.println("<10>");
+  delay(3000);
+  Serial1.println("<08>");
+  Serial1.println("<09>");
+}
+
+
