aa733ad485a3843d0f2af3426d542270c69b762f
Daniel McCool <daniel.mccool@orangepiel.com>
2013-08-08 23:11:52 -0700

    update ids to be ints such that they will be read appropriately into the array

--- broadcast_name/broadcast_name.ino
+++ broadcast_name/broadcast_name.ino
@@ -4,25 +4,25 @@ void setup()
 }
 void loop()
 {
-  Serial1.println("<01>");
+  Serial1.println("<1>");
   delay(1000);
-  Serial1.println("<02>");
+  Serial1.println("<2>");
   delay(1000);
-  Serial1.println("<03>");
+  Serial1.println("<3>");
   delay(1000);
-  Serial1.println("<04>");
+  Serial1.println("<4>");
   delay(1000);
-  Serial1.println("<05>");
+  Serial1.println("<5>");
   delay(10000);
-  Serial1.println("<06>");
+  Serial1.println("<6>");
   delay(5000);
-  Serial1.println("<07>");
+  Serial1.println("<7>");
   delay(8000);
   Serial1.println("<10>");
   delay(3000);
-  Serial1.println("<08>");
-  Serial1.println("<09>");
-  Serial1.println("<09>");
+  Serial1.println("<8>");
+  Serial1.println("<9>");
+  Serial1.println("<9>");
 }
 
 
